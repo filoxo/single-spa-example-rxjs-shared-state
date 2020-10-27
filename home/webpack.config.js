7814c4bdc946e9564c81e848c454f4ff4d16d341
Carlos Filoteo <c.filoteo12@gmail.com>
2020-10-27 15:24:14 -0600

    Init home

---
+++ home/webpack.config.js
@@ -0,0 +1,15 @@
+const webpackMerge = require("webpack-merge");
+const singleSpaDefaults = require("webpack-config-single-spa-react");
+
+module.exports = (webpackConfigEnv, argv) => {
+  const defaultConfig = singleSpaDefaults({
+    orgName: "example",
+    projectName: "home",
+    webpackConfigEnv,
+    argv,
+  });
+
+  return webpackMerge.smart(defaultConfig, {
+    // modify the webpack config however you'd like to by adding to this object
+  });
+};
