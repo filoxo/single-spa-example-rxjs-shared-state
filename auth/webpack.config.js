8df1cf8291b2f52cb4bbfb8e20402311db9b199a
Carlos Filoteo <c.filoteo12@gmail.com>
2020-10-27 15:45:12 -0600

    Assign unique ports

--- auth/webpack.config.js
+++ auth/webpack.config.js
@@ -11,5 +11,8 @@ module.exports = (webpackConfigEnv, argv) => {
 
   return webpackMerge.smart(defaultConfig, {
     // modify the webpack config however you'd like to by adding to this object
+    devServer: {
+      port: 9001,
+    },
   });
 };
