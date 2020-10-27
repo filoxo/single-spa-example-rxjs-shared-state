501f00bf8d93e2b77dc7a062e4a4015ebbdf11ed
Carlos Filoteo <c.filoteo12@gmail.com>
2020-10-27 14:38:30 -0600

    Init root-config

---
+++ root-config/webpack.config.js
@@ -0,0 +1,40 @@
+const webpackMerge = require("webpack-merge");
+const singleSpaDefaults = require("webpack-config-single-spa");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = (webpackConfigEnv, argv) => {
+  const orgName = "example";
+  const defaultConfig = singleSpaDefaults({
+    orgName,
+    projectName: "root-config",
+    webpackConfigEnv,
+    argv,
+  });
+
+  const merge = webpackMerge({
+    customizeArray: webpackMerge.unique(
+      "plugins",
+      ["HtmlWebpackPlugin"],
+      (plugin) => plugin.constructor && plugin.constructor.name
+    ),
+  });
+
+  return merge(
+    {
+      plugins: [
+        new HtmlWebpackPlugin({
+          inject: false,
+          template: "src/index.ejs",
+          templateParameters: {
+            isLocal: webpackConfigEnv && webpackConfigEnv.isLocal === "true",
+            orgName,
+          },
+        }),
+      ],
+    },
+    defaultConfig,
+    {
+      // modify the webpack config however you'd like to by adding to this object
+    }
+  );
+};
