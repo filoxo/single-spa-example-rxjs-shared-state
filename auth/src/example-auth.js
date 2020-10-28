5fb23b962db80f19da3790aacbcbd8a6296cea51
Carlos Filoteo <c.filoteo12@gmail.com>
2020-10-28 11:47:05 -0600

    Implement basic auth subscription

--- auth/src/example-auth.js
+++ auth/src/example-auth.js
@@ -1,4 +1,51 @@
-import "./set-public-path";
+import { BehaviorSubject } from "rxjs";
 
-// Anything exported from this file is importable by other in-browser modules.
-export function publicApiFunction() {}
+export const auth$ = new BehaviorSubject({
+  sessionToken: localStorage.getItem("sessionToken"),
+  error: false,
+  pending: false,
+});
+
+// This promise represents a request being made to some backend to have the user validated and logged in
+// but is mocked here for convenience. I don't want to have to setup a backend just for this example.
+const GET_LOGGED_IN = (username, password) =>
+  new Promise((resolve, reject) => {
+    auth$.next({
+      sessionToken: null,
+      error: false,
+      pending: true,
+    });
+    setTimeout(() => {
+      if (username === "exampleuser" && password === "examplepassword") {
+        resolve({
+          sessionToken: "abc123def456",
+          error: false,
+          pending: false,
+        });
+      } else {
+        // Why resolve when invalid? Because the "backend" provided a valid response
+        resolve({
+          sessionToken: null,
+          error: "Invalid user or password",
+          pending: false,
+        });
+      }
+    }, 4000);
+  });
+
+export function login(username, password) {
+  if (!auth$.value.pending) {
+    GET_LOGGED_IN(username, password).then((user) => {
+      auth$.next(user);
+    });
+  }
+}
+
+export function logout() {
+  // Trigger side-effects
+  localStorage.removeItem("sessionToken");
+  auth$.next({
+    sessionToken: null,
+    error: false,
+  });
+}
