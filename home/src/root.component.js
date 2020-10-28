60c3640130bfcddf3e5e3b31768fb29d983d8079
Carlos Filoteo <c.filoteo12@gmail.com>
2020-10-28 14:26:27 -0600

    Add basic home styles

--- home/src/root.component.js
+++ home/src/root.component.js
@@ -1,5 +1,9 @@
 import React from "react";
 
 export default function Root(props) {
-  return <section>{props.name} is mounted!</section>;
+  return (
+    <section style={{ textAlign: "center", padding: "1rem" }}>
+      {props.name} is mounted!
+    </section>
+  );
 }
