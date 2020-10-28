5c22ece59222733fa65094b722c9da0677beb21a
Carlos Filoteo <c.filoteo12@gmail.com>
2020-10-28 13:26:00 -0600

    Add logout button; redirect if not logged in

--- navbar/src/App.svelte
+++ navbar/src/App.svelte
@@ -1,5 +1,18 @@
 <script>
-  import { Router, Link } from 'svelte-routing';
+  import { onDestroy, onMount } from 'svelte';
+  import { Router, Link, navigate } from 'svelte-routing';
+  import { auth$ as auth, logout } from '@example/auth';
+
+  let sub;
+  onMount(() => {
+    sub = auth.subscribe(({ sessionToken }) => {
+      if (!sessionToken) navigate('/login');
+    });
+  });
+
+  onDestroy(() => {
+    sub.unsubscribe();
+  });
 </script>
 
 <style>
@@ -9,12 +22,20 @@
     padding: 1rem 2rem;
   }
 
-  nav :global(a) {
+  nav :global(a),
+  nav .action {
+    background: transparent;
     border: 1px solid transparent;
     border-radius: 3px;
     color: white;
+    font-size: 1rem;
     padding: 0.25rem 0.5rem;
     text-decoration: none;
+  }
+
+  nav .action:hover,
+  nav .action:focus {
+    background: rgba(0, 0, 0, 0.3);
   }
 
   nav :global(a:focus),
@@ -28,7 +49,14 @@
 
 <nav>
   <Router>
-    <Link to="/home">Home</Link>
-    <Link to="/login">Login</Link>
+    {#if $auth.sessionToken}
+      <Link to="/home">Home</Link>
+      <button
+        class="action"
+        type="button"
+        on:click|once={logout}>Logout</button>
+    {:else}
+      <Link to="/login">Login</Link>
+    {/if}
   </Router>
 </nav>
