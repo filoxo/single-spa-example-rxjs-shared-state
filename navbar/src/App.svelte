2e6b6bad1616250add3ac66e0578bc6ef1835f13
Carlos Filoteo <c.filoteo12@gmail.com>
2020-10-28 09:17:27 -0600

    Add basic routing and links to navbar

--- navbar/src/App.svelte
+++ navbar/src/App.svelte
@@ -1,11 +1,34 @@
 <script>
-  export let name;
+  import { Router, Link } from 'svelte-routing';
 </script>
 
 <style>
-  section {
-    font-size: 1.5rem;
+  nav {
+    background: crimson;
+    color: white;
+    padding: 1rem 2rem;
+  }
+
+  nav :global(a) {
+    border: 1px solid transparent;
+    border-radius: 3px;
+    color: white;
+    padding: 0.25rem 0.5rem;
+    text-decoration: none;
+  }
+
+  nav :global(a:focus),
+  nav :global(a:hover) {
+    text-decoration: underline;
+  }
+  nav :global(a[aria-current]) {
+    border-color: white;
   }
 </style>
 
-<section>{name} is mounted!</section>
+<nav>
+  <Router>
+    <Link to="/home">Home</Link>
+    <Link to="/login">Login</Link>
+  </Router>
+</nav>
