ad67ae3690ee4b6f65f0482de9bcbd3fb0218a75
Carlos Filoteo <c.filoteo12@gmail.com>
2020-10-28 11:45:51 -0600

    Enable lazyStyleTags

--- login/webpack.config.js
+++ login/webpack.config.js
@@ -1,5 +1,19 @@
-const webpackMerge = require("webpack-merge");
+const { mergeWithRules } = require("webpack-merge");
 const singleSpaDefaults = require("webpack-config-single-spa-react");
+
+const mergeRulesByTestMatch = mergeWithRules({
+  module: {
+    rules: {
+      test: "match",
+      include: "replace",
+      exclude: "replace",
+      use: {
+        loader: "match",
+        options: "replace"
+      }
+    }
+  }
+});
 
 module.exports = (webpackConfigEnv, argv) => {
   const defaultConfig = singleSpaDefaults({
@@ -9,10 +23,28 @@ module.exports = (webpackConfigEnv, argv) => {
     argv,
   });
 
-  return webpackMerge.smart(defaultConfig, {
+  const config = mergeRulesByTestMatch(defaultConfig, {
     // modify the webpack config however you'd like to by adding to this object
+    module: {
+      rules: [
+        {
+          test: /\.css$/i,
+          include: undefined,
+          exclude: /\.lazy\.css$/i
+        },
+        {
+          test: /\.lazy\.css$/i,
+          use: [
+            { loader: "style-loader", options: { injectType: "lazyStyleTag" } },
+            "css-loader",
+          ],
+        }
+      ]
+    },
     devServer: {
       port: 5002,
     },
   });
+
+  return config;
 };
