f3d17e6809324cf4bed93c7e27dba069ce47cb5c
Carlos Filoteo <c.filoteo12@gmail.com>
2021-01-28 08:23:04 -0700

    Add entry output message for home

--- packages/home/webpack.config.js
+++ packages/home/webpack.config.js
@@ -13,6 +13,16 @@ module.exports = (webpackConfigEnv, argv) => {
     // modify the webpack config however you'd like to by adding to this object
     devServer: {
       port: 5001,
+      onListening: ({ compiler }) => {
+        const { https, client } = compiler.options.devServer;
+        const { publicPath, filename } = compiler.options.output;
+        const protocol = https ? "https://" : "http://";
+        const port = !client.port ? "" : `:${client.port}`;
+        const path = ["", "auto"].includes(publicPath) ? "/" : publicPath;
+        console.log(
+          `⚡️ single-spa application entry: ${protocol}${client.host}${port}${path}${filename}`
+        );
+      },
     },
   });
 };
