eb87af14950d9f807aa451e57aaa3ddb19b16128
Carlos Filoteo <c.filoteo12@gmail.com>
2021-01-28 07:53:13 -0700

    Update webpack config

--- packages/root-config/webpack.config.js
+++ packages/root-config/webpack.config.js
@@ -1,40 +1,37 @@
-const webpackMerge = require("webpack-merge");
-const singleSpaDefaults = require("webpack-config-single-spa");
-const HtmlWebpackPlugin = require("html-webpack-plugin");
+const { mergeWithCustomize, unique } = require('webpack-merge');
+const singleSpaDefaults = require('webpack-config-single-spa');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+const merge = mergeWithCustomize({
+  customizeArray: unique(
+    'plugins',
+    ['HtmlWebpackPlugin'],
+    (plugin) => plugin.constructor && plugin.constructor.name
+  ),
+});
 
 module.exports = (webpackConfigEnv, argv) => {
-  const orgName = "example";
+  const orgName = 'example';
   const defaultConfig = singleSpaDefaults({
     orgName,
-    projectName: "root-config",
+    projectName: 'root-config',
     webpackConfigEnv,
     argv,
   });
 
-  const merge = webpackMerge({
-    customizeArray: webpackMerge.unique(
-      "plugins",
-      ["HtmlWebpackPlugin"],
-      (plugin) => plugin.constructor && plugin.constructor.name
-    ),
+  const config = merge(defaultConfig, {
+    // modify the webpack config however you'd like to by adding to this object
+    plugins: [
+      new HtmlWebpackPlugin({
+        inject: false,
+        template: 'src/index.ejs',
+        templateParameters: {
+          isLocal: webpackConfigEnv && webpackConfigEnv.isLocal,
+          orgName,
+        },
+      }),
+    ],
   });
 
-  return merge(
-    {
-      plugins: [
-        new HtmlWebpackPlugin({
-          inject: false,
-          template: "src/index.ejs",
-          templateParameters: {
-            isLocal: webpackConfigEnv && webpackConfigEnv.isLocal === "true",
-            orgName,
-          },
-        }),
-      ],
-    },
-    defaultConfig,
-    {
-      // modify the webpack config however you'd like to by adding to this object
-    }
-  );
+  return config;
 };
