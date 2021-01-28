11ac4a481b6b0f5e9d84e7c2a10e35f11bca58e1
Carlos Filoteo <c.filoteo12@gmail.com>
2021-01-28 08:14:12 -0700

    Add entry output message for auth

--- packages/auth/webpack.config.js
+++ packages/auth/webpack.config.js
@@ -11,9 +11,19 @@ module.exports = (webpackConfigEnv, argv) => {
 
   return merge(defaultConfig, {
     // modify the webpack config however you'd like to by adding to this object
+    externals: ["rxjs"],
     devServer: {
       port: 9001,
+      onListening: ({ compiler }) => {
+        const { https, client } = compiler.options.devServer;
+        const { publicPath, filename } = compiler.options.output;
+        const protocol = https ? "https://" : "http://";
+        const port = !client.port ? "" : `:${client.port}`;
+        const path = ["", "auto"].includes(publicPath) ? "/" : publicPath;
+        console.log(
+          `⚡️ single-spa application entry: ${protocol}${client.host}${port}${path}${filename}`
+        );
+      },
     },
-    externals: ["rxjs"],
   });
 };
